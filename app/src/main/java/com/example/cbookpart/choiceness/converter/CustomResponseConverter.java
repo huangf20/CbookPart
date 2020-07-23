ff9577a64d6a8f77a3e4657bcb059ec3e87f97bb
huangf20 <huangfeng@zhaoxitech.com>
2020-07-23 15:24:24 +0800

    优化CustomResponseConverter中代码，重复代码合并

--- app/src/main/java/com/example/cbookpart/choiceness/converter/CustomResponseConverter.java
+++ app/src/main/java/com/example/cbookpart/choiceness/converter/CustomResponseConverter.java
@@ -15,7 +15,8 @@ import com.example.cbookpart.choiceness.data.itemBean.BookItemBean;
 import com.example.cbookpart.choiceness.data.itemBean.EntryItemBean;
 
 import java.io.IOException;
-import java.lang.reflect.Array;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 
@@ -32,12 +33,25 @@ public class CustomResponseConverter implements Converter<ResponseBody, ResultBe
     private int featureValues;
     private Feature[] features;
 
+    private ArrayList<String> mTypes = new ArrayList<>();
+    private ArrayList<Class<? extends BaseItemBean>> mItemClasses = new ArrayList<>();
+    private ArrayList<Class<? extends ModuleBean>> mModuleClasses = new ArrayList<>();
+
     CustomResponseConverter(Type type, ParserConfig config, int featureValues,
                             Feature... features) {
         mType = type;
         this.config = config;
         this.featureValues = featureValues;
         this.features = features;
+        mTypes.add("banner");
+        mTypes.add("entry");
+        mTypes.add("booklist");
+        mItemClasses.add(BannerItemBean.class);
+        mItemClasses.add(EntryItemBean.class);
+        mItemClasses.add(BookItemBean.class);
+        mModuleClasses.add(BannerModuleBean.class);
+        mModuleClasses.add(EntryModuleBean.class);
+        mModuleClasses.add(BookModuleBean.class);
     }
 
     @Override
@@ -53,50 +67,36 @@ public class CustomResponseConverter implements Converter<ResponseBody, ResultBe
                 ArrayList<BaseItemBean> baseItemBeans = moduleBean.getItems();
                 ArrayList<BaseItemBean> replaceItems = new ArrayList<>();
                 String type = moduleBean.getType();
-                for (int j = 0; j < baseItemBeans.size(); j++) {
-                    Object object = baseItemBeans.get(j);
-                    JSONObject jsonObject = (JSONObject) object;
-                    if (type.equals("banner")) {
-                        BannerItemBean bannerItemBean = JSONObject.toJavaObject(jsonObject, BannerItemBean.class);
-                        replaceItems.add(bannerItemBean);
+                for (String mtype : mTypes) {
+                    if (type.equals(mtype)) {
+                        int index = mTypes.indexOf(mtype);
+                        for (int j = 0; j < baseItemBeans.size(); j++) {
+                            Object object = baseItemBeans.get(j);
+                            JSONObject jsonObject = (JSONObject) object;
+                            replaceItems.add(JSONObject.toJavaObject(jsonObject, mItemClasses.get(index)));
+                        }
+                        moduleBean.setItems(replaceItems);
+                        Constructor constructor = mModuleClasses.get(index).getConstructor(ModuleBean.class);
+                        replaceModules.add((ModuleBean) constructor.newInstance(moduleBean));
                     }
-                    if (type.equals("entry")) {
-                        EntryItemBean entryItemBean = JSONObject.toJavaObject(jsonObject, EntryItemBean.class);
-                        replaceItems.add(entryItemBean);
-                    }
-                    if (type.equals("booklist")) {
-                        BookItemBean bookItemBean = JSONObject.toJavaObject(jsonObject, BookItemBean.class);
-                        replaceItems.add(bookItemBean);
-                    }
+                }
 
-
-                }
-                if (type.equals("banner")) {
-                    BannerModuleBean bannerModuleBean = new BannerModuleBean(moduleBean.getId(), moduleBean.getType(), moduleBean.getTitle(), moduleBean.getChannel(), moduleBean.isShowMore(), moduleBean.getShowType(), moduleBean.getShowNum(),
-                            moduleBean.getStartTime(), moduleBean.getEndTime(), moduleBean.getLinkUrl(), moduleBean.isCanDownload(), moduleBean.isCountDownNotShow(), moduleBean.getBtn(), moduleBean.getTargetType(),
-                            replaceItems, moduleBean.getCanReceiveTimes());
-                    replaceModules.add(bannerModuleBean);
-                }
-                if (type.equals("entry")) {
-                    EntryModuleBean entryModuleBean = new EntryModuleBean(moduleBean.getId(), moduleBean.getType(), moduleBean.getTitle(), moduleBean.getChannel(), moduleBean.isShowMore(), moduleBean.getShowType(), moduleBean.getShowNum(),
-                            moduleBean.getStartTime(), moduleBean.getEndTime(), moduleBean.getLinkUrl(), moduleBean.isCanDownload(), moduleBean.isCountDownNotShow(), moduleBean.getBtn(), moduleBean.getTargetType(),
-                            replaceItems, moduleBean.getCanReceiveTimes());
-                    replaceModules.add(entryModuleBean);
-                }
-                if (type.equals("booklist")) {
-                    BookModuleBean bookModuleBean = new BookModuleBean(moduleBean.getId(), moduleBean.getType(), moduleBean.getTitle(), moduleBean.getChannel(), moduleBean.isShowMore(), moduleBean.getShowType(), moduleBean.getShowNum(),
-                            moduleBean.getStartTime(), moduleBean.getEndTime(), moduleBean.getLinkUrl(), moduleBean.isCanDownload(), moduleBean.isCountDownNotShow(), moduleBean.getBtn(), moduleBean.getTargetType(),
-                            replaceItems, moduleBean.getCanReceiveTimes());
-
-                    replaceModules.add(bookModuleBean);
-                }
 
             }
             resultBean.getValue().get(0).setModules(replaceModules);
 
             return resultBean;
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
         } finally {
             value.close();
         }
+        return null;
     }
 }
--- app/src/main/java/com/example/cbookpart/choiceness/data/ModuleBean/BannerModuleBean.java
+++ app/src/main/java/com/example/cbookpart/choiceness/data/ModuleBean/BannerModuleBean.java
@@ -4,8 +4,24 @@ import com.example.cbookpart.choiceness.data.itemBean.BannerItemBean;
 
 import java.util.ArrayList;
 
-public class BannerModuleBean extends ModuleBean{
-    public BannerModuleBean() {
+public class BannerModuleBean extends ModuleBean<BannerItemBean>{
+    public BannerModuleBean(ModuleBean moduleBean) {
+        this.id=moduleBean.getId();
+        this.type=moduleBean.getType();
+        this.title=moduleBean.getTitle();
+        this.channel=moduleBean.getChannel();
+        this.showMore=moduleBean.isShowMore();
+        this.showType=moduleBean.getShowType();
+        this.showNum=moduleBean.getShowNum();
+        this.startTime=moduleBean.getStartTime();
+        this.endTime=moduleBean.getEndTime();
+        this.linkUrl=moduleBean.getLinkUrl();
+        this.canDownload=moduleBean.isCanDownload();
+        this.countDownNotShow=moduleBean.isCountDownNotShow();
+        this.btn=moduleBean.getBtn();
+        this.targetType=moduleBean.getTargetType();
+        this.items=moduleBean.getItems();
+        this.canReceiveTimes=moduleBean.getCanReceiveTimes();
     }
 
     public BannerModuleBean(int id, String type, String title, String channel, boolean showMore, String showType, int showNum, int startTime, int endTime, String linkUrl, boolean canDownload, boolean countDownNotShow, String btn, String targetType, ArrayList items, String canReceiveTimes) {
